4ef6732de68104fb94a256b6745407cbed686b63
Arthur <Arthur@172.27.77.214>
2018-03-11 16:08:37 +0100

    test

--- src/jeu/Main.java
+++ src/jeu/Main.java
@@ -15,7 +15,8 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
-public class Main extends Application { // Fonctionne ?
+public class Main extends Application { // ça Fonctionne ?????
+	// EDJNEIJ
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
