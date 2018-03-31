3487596379fe6d4f1f10bf7edaf5ac1748fd0817
Arthur <Arthur@172.20.10.3>
2018-03-31 22:49:10 +0200

    Trois nouvelles classes pour le déplacement et pl court chem

---
+++ src/jeu/CreateGrapheDepl.java
@@ -0,0 +1,83 @@
+package jeu;
+
+public class CreateGrapheDepl {
+	
+	int depl;
+	int[][] cases;
+	int firstcase;
+	int centre;
+	int Nmatricedepl;
+	Map map;
+	
+	public CreateGrapheDepl(int depl,int firstcase,int centre, int Nmatricedepl, Map map) {
+		this.depl=depl;
+		this.firstcase=firstcase;
+		this.centre=centre;
+		this.Nmatricedepl=Nmatricedepl;
+		this.map=map;
+		this.cases=transforme();
+	}
+	
+	Graphe generate() {
+		int taille = (2*depl+1)*(2*depl+1);
+		Graphe g = new Graphe(taille);
+		for (int i=0;i<2*depl+1;i++) {
+			for (int j=0;j<2*depl+1;j++) {
+				int graphecase = i*(2*depl+1)+j;
+				g.ajouterSommet(graphecase);
+			}
+		}
+		for (int i=1;i<2*depl;i++) { // haut
+			g.ajouterLiaisons(i,i+(2*depl+1),cases[0][i]);
+		}
+		for (int i=1;i<2*depl;i++) { // bas
+			g.ajouterLiaisons(i+(2*depl)*(2*depl+1),i+(2*depl-1)*(2*depl+1),cases[2*depl][i]);
+		}
+		for (int i=1;i<2*depl;i++) { // gauche
+			g.ajouterLiaisons(i*(2*depl+1),i*(2*depl+1)+1,cases[i][0]);
+		}
+		for (int i=1;i<2*depl;i++) { // droite
+			g.ajouterLiaisons(i*(2*depl+1)+(2*depl+1),i*(2*depl+1)+(2*depl+1)-1,cases[i][2*depl]);
+		}
+		for (int i=1;i<2*depl;i++) {
+			for (int j=1;j<2*depl;j++) {
+				int graphecase = i*(2*depl+1)+j;
+				g.ajouterLiaisons(graphecase,i*(2*depl+1)+j+1,cases[i][j+1]);
+				g.ajouterLiaisons(graphecase,i*(2*depl+1)+j-1,cases[i][j-1]);
+				g.ajouterLiaisons(graphecase,(i+1)*(2*depl+1)+j,cases[i+1][j]);
+				g.ajouterLiaisons(graphecase,(i-1)*(2*depl+1)+j,cases[i-1][j]);
+			}
+		}
+		return g;
+	}
+	
+	int[][] transforme() {
+		int N = 2*depl+1;
+		int NN = Nmatricedepl;
+		int[][] res = new int[N][N];
+		for (int i=0;i<N;i++) {
+			for (int j=0;j<N;j++) {
+				res[i][j]=map.plateau[i*NN+j].terrain.deplacement;
+			}
+		}
+		return res;
+	}
+	
+	void reversechemin(int[] chemin) {
+		int longueur = 0;
+		int i=0;
+		while ((chemin[i]!=-1)&&(i<chemin.length)) {
+			longueur++;
+			i++;
+		}
+		int[] res = new int[longueur];
+		for (int j=0;j<longueur/2;j++) {
+			res[j]=chemin[longueur-1-j];
+			res[longueur-1-j]=chemin[j];
+		}
+		chemin=res;
+	}
+	
+	
+
+}
---
+++ src/jeu/Dijkstra.java
@@ -0,0 +1,120 @@
+package jeu;
+
+public class Dijkstra {
+	
+	Graphe g;
+	int sommetcible;
+	int sommetdepart;
+	int[][] sommets; // { {distance,provenance,visité (1 non ou 0 oui)},...}
+	int nbsommets;
+	static final int INF = 1000;
+	public final static int vide = -999;
+	
+	Dijkstra(Graphe g, int sommetdepart, int sommetcible) {
+		this.g = g;
+		this.sommetcible=sommetcible;
+		this.sommetdepart =sommetdepart;
+		nbsommets = g.nbSommet();
+		sommets = new int[nbsommets][3];
+		
+		for (int k=0;k<nbsommets;k++) {
+			sommets[k][0] = INF;
+			sommets[k][2] = 1;
+		}
+		
+		sommets[sommetdepart][0] = 0;
+		sommets[sommetdepart][1] = sommetdepart;
+		sommets[sommetdepart][2] = 1;
+		
+	}
+	
+	void changeGrapheDij(Graphe g) {
+		this.g = g;
+	}
+	
+	int runDijkstra() {
+		int sommetenvisite = sommetdepart;
+		displaytot(sommets);
+		while (sommets[sommetcible][2]==1) {
+			System.out.println(sommetenvisite);
+			visiteSommet(sommetenvisite);
+			displaytot(sommets);
+			sommetenvisite = plusPetitNonVisité();
+			if (sommetenvisite==-1) {
+				return sommets[sommetcible][0];
+			}
+		}
+		return sommets[sommetcible][0];
+	}
+	
+	int pluspetitsommet() {
+		int res = INF;
+		int rang = sommetdepart;
+		for(int i = 0; i<sommets.length;i++) {
+			if (res > sommets[i][0]) {
+				res = sommets[i][0];
+				rang = i;
+			}
+		}
+		return rang;
+	}
+	
+	void visiteSommet(int sommet) {
+		int poids = sommets[sommet][0];
+		for (int i = 0; i<g.valid.length;i++) {
+			int a = g.liaisons[sommet][i];
+			if (g.valideLiaison(sommet, i)) {
+				if ((poids+a<sommets[i][0])&&(sommets[i][2]==1)) {
+					sommets[i][0]=poids+a;
+					sommets[i][1]=sommet;
+				}
+			}
+		}
+		sommets[sommet][2]=0;
+	}
+	
+	int plusPetitNonVisité() {
+		int res = INF;
+		int rang = -1;
+		for (int i = 0;i<sommets.length;i++) {
+			if ((sommets[i][0]<res)&&(sommets[i][2]==1)) {
+				res = sommets[i][0];
+				rang = i;
+			}
+		}
+		return rang;
+	}
+	
+	void displaytot(int[][] tab) {
+		System.out.print("{");
+		display(tab[0]);
+		for (int i = 1; i<tab.length;i++) {
+			System.out.print(", ");
+			display(tab[i]);
+		}
+		System.out.println("}");
+	}
+	
+	void display(int[] tab) {
+		System.out.print("{" + tab[0]);
+		for (int i = 1; i<tab.length;i++) {
+			System.out.print(", " + tab[i]);
+		}
+		System.out.print("}");
+	}
+	
+	int[] chemin() {
+		int[] chemin = new int[sommets.length];
+		int suiveurchemin = sommetcible;
+		chemin[0]=sommetcible;
+		int k = 1;
+		while (suiveurchemin != sommetdepart) {
+			suiveurchemin = sommets[suiveurchemin][1];
+			chemin[k]=suiveurchemin;
+			k++;
+		}
+		return chemin;
+	}
+	
+
+}
