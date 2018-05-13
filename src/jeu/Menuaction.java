054093e93532d1e92baa0787f19934330c3d71c9
Popes <Popes@157.159.45.49>
2018-05-13 16:24:20 +0200

    Changement affichage menu pour les healer

--- src/jeu/Menuaction.java
+++ src/jeu/Menuaction.java
@@ -6,8 +6,10 @@ import javafx.scene.image.Image;
 public class Menuaction {
 	/**Image de fond du menu lateral droit**/
 	Image menucache;
-	/**Image de fond du menu lateral droit**/
-	Image menu1;
+	/**Image du menu d'action quand l'unite attaque**/
+	Image menu1attaque;
+	/**Image du menu d'action quand l'unite soigne**/
+	Image menu1soin;
 	/**Image de fond du menu changer tour**/
 	Image menu2;
 	/**Image du curseur pour les menu**/
@@ -15,14 +17,17 @@ public class Menuaction {
 	/**Cache des options qui ne sont plus valide */
 	Image cache;
 	int positionxmenu;
+	int positionymenu;
 	GraphicsContext gc;
 	/**Position du curseur dans le menu 0 : attaquer; 1 : deplacer; 2 : capturer*/
 	int positioncurseur1;
 	
 	Menuaction(GraphicsContext gc, int positionxmenu,int width, int height){
 		this.positionxmenu=positionxmenu;
+		positionymenu= 90;
 		menucache = new Image("wood.jpg", width-positionxmenu,height,false,false);
-		menu1 = new Image("menu1(10x16).png", 200, 320, false, false);
+		menu1attaque = new Image("menu1attaque(10x16).png", 200, 320, false, false);
+		menu1soin = new Image("menu1soin(10x16).png", 200, 320, false, false);
 		curseur = new Image("curseurmenu1.png",200, 320, false, false);
 		menu2 = new Image("menu2(10x16).jpg", 200, 320, false, false);
 		cache = new Image("cache.png",200, 320, false, false);
@@ -40,18 +45,23 @@ public class Menuaction {
 		jeu.gc.drawImage(menucache, positionxmenu, 0);
 	    switch(jeu.menu) {
 	    case 1:
-				jeu.gc.drawImage(menu1, positionxmenu*1.05, 50);
+	    		if (jeu.map.selectionnemenu.unite.type.compareTo("healer")!=0){
+	    			jeu.gc.drawImage(menu1attaque, positionxmenu*1.05, positionymenu);
+	    		}
+	    		else {
+	    			jeu.gc.drawImage(menu1soin, positionxmenu*1.05, positionymenu);
+	    		}
 				if (jeu.map.selectionnemenu.unite.restdeplacement == 0) {
-	    			jeu.gc.drawImage(cache, positionxmenu*1.05,50+1*52);
+	    			jeu.gc.drawImage(cache, positionxmenu*1.05,positionymenu+1*52);
 	    		}
 				if ((jeu.map.selectionnemenu.batiment == null)||(jeu.map.selectionnemenu.batiment.joueur == jeu.map.selectionnemenu.unite.joueur)) {
-					jeu.gc.drawImage(cache, positionxmenu*1.05,50+2*52);
+					jeu.gc.drawImage(cache, positionxmenu*1.05,positionymenu+2*52);
 				}
-				gc.drawImage(curseur, positionxmenu*1.05,50+positioncurseur1*52);
+				gc.drawImage(curseur, positionxmenu*1.05,positionymenu+positioncurseur1*52);
 				break;
 	    case 2:
-	    		jeu.gc.drawImage(menu2, positionxmenu*1.05, 50);
-	    		jeu.gc.drawImage(curseur, positionxmenu*1.05,50+positioncurseur1*52);
+	    		jeu.gc.drawImage(menu2, positionxmenu*1.05, positionymenu);
+	    		jeu.gc.drawImage(curseur, positionxmenu*1.05,positionymenu+positioncurseur1*52);
 	    		break;
 	
 	    default:
